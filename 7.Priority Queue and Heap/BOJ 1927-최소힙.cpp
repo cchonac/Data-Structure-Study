75ffed114a889f4cc55ea7409defb6ccd09ffe76
Yoon Hyuck Cho <hiyoonhyuck@naver.com>
2023-02-25 22:26:34 +0900

    update

---
+++ 7.Priority Queue and Heap/BOJ 1927-최소힙.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+/*void intSwap(int& a,int& b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+
+}*/
+int main(void){
+    int a=10;
+    int b=20;
+    //intSwap(a,b);
+    cout<<a<<b<<endl;
+    return 0;
+
+}
